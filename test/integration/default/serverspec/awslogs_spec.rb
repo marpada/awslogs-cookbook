03d7bde96f12067fee06555461dac516de6c0adc
David Pando <david.pando@gmail.com>
2015-09-15 15:59:47 +1200

    Tests for streams attributes

--- test/integration/default/serverspec/awslogs_spec.rb
+++ test/integration/default/serverspec/awslogs_spec.rb
@@ -14,6 +14,10 @@ describe file("/var/awslogs/etc/awslogs.conf") do
   it { should exist }
   it { should be_file }
   it { should be_owned_by 'root' }
+  its(:content) { should match /\[ test.log \]/ }
+  its(:content) { should match /file = \/var\/log\/test.log/ }
+  its(:content) { should match /log_stream_name = {instance_id}/ }
+  its(:content) { should match /datetime_format = %Y-%m-%d %H:%M:%S/ }
 end
 
 describe file("/var/log/awslogs.log") do
