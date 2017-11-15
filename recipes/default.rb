6f6882b2a3642a4ad21ee60ed11a3cdc4ab8bd65
yfleisch18 <yfleisch18@gmail.com>
2017-11-15 17:37:26 -0500

    If using < ubuntu 15.04 uses the init.d service, if higher, uses systemd

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 %w{bin etc lib local state}.each do |f|
   directory "#{node['awslogs_agent']['path']}/#{f}" do
     owner node['awslogs_agent']['user']
@@ -23,24 +24,47 @@ python_virtualenv node['awslogs_agent']['path'] do
   group node['awslogs_agent']['group']
 end
 
-
 python_package "awscli-cwlogs" do
   virtualenv node['awslogs_agent']['path']
   version node['awslogs_agent']['version']
   options "--extra-index-url=#{node['awslogs_agent']['plugin_url']}"
 end
 
-template "/etc/init/#{node['awslogs_agent']['service']}.conf" do
+destination_folder = ''
+source_file = ''
+if  'ubuntu' == node['platform']
+  if Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
+    destination_folder = "/lib/systemd/system/#{node['awslogs_agent']['service']}.service"
+    source_file = "etc/systemd/awslogs_daemon.service.erb"
+  elsif Chef::VersionConstraint.new('>= 12.04').include?(node['platform_version'])
+    destination_folder = "/etc/init/#{node['awslogs_agent']['service']}.conf"
+    source_file = "etc/init/awslogs.conf.erb"
+  end
+end
+
+template destination_folder do
   owner 'root'
   group 'root'
   mode '0644'
-  source 'etc/init/awslogs.conf.erb'
+  source source_file
   variables(
     :user => node['awslogs_agent']['user'],
     :path => node['awslogs_agent']['path']
   )
   notifies :restart, "service[#{node['awslogs_agent']['service']}]"
 end
+
+template "#{node['awslogs_agent']['path']}/bin/awslogs-agent-launcher.sh" do
+  source 'var/awslogs-agent-launcher.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables(
+    :user => node['awslogs_agent']['user'],
+    :path => node['awslogs_agent']['path']
+  )
+end
+
 
 if node['awslogs_agent']['databag']
   aws = data_bag_item(node['awslogs_agent']['databag'], "main")
@@ -75,9 +99,18 @@ template "#{node['awslogs_agent']['path']}/etc/awslogs.conf" do
   notifies :restart, "service[#{node['awslogs_agent']['service']}]"
 end
 
+service_provider = ''
+if  'ubuntu' == node['platform']
+  if Chef::VersionConstraint.new('>= 15.04').include?(node['platform_version'])
+    service_provider = Chef::Provider::Service::Systemd
+  elsif Chef::VersionConstraint.new('>= 12.04').include?(node['platform_version'])
+    service_provider = Chef::Provider::Service::Upstart
+  end
+end
+
 service node['awslogs_agent']['service'] do
-  action [:start, :enable]
-  provider Chef::Provider::Service::Upstart
+  action [:enable, :start]
+  provider service_provider
 end
 
 link '/var/log/awslogs.log' do
